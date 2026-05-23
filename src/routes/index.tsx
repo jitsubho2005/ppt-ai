7ebdfe9d0a43ac28c83c8721a7ea418cc7ed3e01
jitsubho2005 <jitsubho2008@gmail.com>
2026-05-23 22:22:57 +0530

    add auth

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Switch } from '#/components/ui/switch'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: Home })
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <Switch></Switch>
+    </div>
+  )
+}
